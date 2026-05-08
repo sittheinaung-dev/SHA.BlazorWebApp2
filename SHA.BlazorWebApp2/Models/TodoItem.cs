a19c097dc2563abca7213b34f7fc14dcfa4e58b3
Administrator <Administrator@HP-LAPTOP>
2026-05-08 16:06:30 +0630

    feat: vercel

--- SHA.BlazorWebApp2/Models/TodoItem.cs
+++ SHA.BlazorWebApp2/Models/TodoItem.cs
@@ -6,7 +6,7 @@ public class TodoItem
 {
     public Guid Id { get; set; } = Guid.NewGuid();
     
-    [Required]
+    [Required] 
     public string Title { get; set; } = string.Empty;
     
     public string Description { get; set; } = string.Empty;
